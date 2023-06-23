0930bacbb148a39fb2e1fa2b1d7990460cf7c378
KOSALAI V <kosalaiv@KOSALAIs-MacBook-Pro.local>
2023-06-23 18:53:49 +0530

    added script

---
+++ statuscode_checker.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+input_file = 'urls_list.xlsx'
+output_file = 'failed_urls.xlsx'
+
+# Load the Excel file containing URLs
+df = pd.read_excel(input_file, header=None)
+
+# Get the number of URLs
+num_urls = len(df)
+print(f"Total URLs: {num_urls}")
+
+# Create a session for HTTP requests
+session = requests.Session()
+
+# Extract URLs from the Excel file
+urls = df.iloc[:, 0].tolist()
+
+# List to store failed URLs
+failed_urls = []
+
+# Process each URL and handle exceptions
+def process_url(url):
+    try:
+        # Send HEAD request and check response status
+        response = session.head(url, timeout=5)
+        response.raise_for_status()
+        if response.status_code == 404 or response.status_code == 500:
+            print(f"FAIL: {url} - {response.status_code}")
+            failed_urls.append(url)
+        else:
+            print(f"PASS: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"ERROR: {url} - {e}")
+        failed_urls.append(url)
+
+# Set the number of concurrent threads
+num_threads = 50
+
+# Create a thread pool executor
+with ThreadPoolExecutor(max_workers=num_threads) as executor:
+    # Process URLs using the thread pool
+    executor.map(process_url, urls)
+
+# Create a DataFrame with failed URLs and save to Excel
+df_failed_urls = pd.DataFrame(failed_urls)
+df_failed_urls.to_excel(output_file, index=False, header=False)
